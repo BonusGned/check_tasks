c134e6c742146a562214079845fb110f174a2d8d
BonusGned <62764420+BonusGned@users.noreply.github.com>
2021-07-09 22:50:36 +0300

    Update settings.py

--- check_tasks/settings.py
+++ check_tasks/settings.py
@@ -9,17 +9,21 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dvlz4+t_7r*%5*a&&k=e48*n_*hmy035w1^au_xw@!jonne23c'
+load_dotenv()
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
